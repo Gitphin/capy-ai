67a96e5b12ed4971ae68647069b12ca42aa99250
Gitphin <alek.holiman@gmail.com>
2024-08-26 17:36:25 -0500

    Scaling fixes

--- src/usertext/UserText.jsx
+++ src/usertext/UserText.jsx
@@ -4,6 +4,7 @@ import styles from './usertext.module.css';
 export const UserText = ({ usertext }) => {
   return (
     <section className={styles.section}>
+      <h2>You: </h2>
       <p className={styles.usertext}>{usertext}</p>
     </section>
   );

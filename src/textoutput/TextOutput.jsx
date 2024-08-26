61596efaf087aedc57f39fb38712f0228b8872a5
Gitphin <alek.holiman@gmail.com>
2024-08-26 14:45:02 -0500

    User input now shows, first step to bringing back convo logs

--- src/textoutput/TextOutput.jsx
+++ src/textoutput/TextOutput.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from './textoutput.module.css';
 import capy from "../../capyicon.png";
 
-export const TextOutput = ({ text }) => {
+export const TextOutput = ({ usertext, text }) => {
   return (
     <section className={styles.section}>
       <div className={styles.static}><img src={capy} alt="Capybara" /></div>
       <p className={styles.text}>{text}</p>
+      <p className={styles.usertext}>{usertext}</p>
     </section>
   );
 };
